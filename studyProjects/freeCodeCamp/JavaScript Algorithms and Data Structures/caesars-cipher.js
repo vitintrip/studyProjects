8f5f2c085311d33ef65355ab6b807b4591dbe5dc
Vitaliy Kravchenko <vk@Vitaliys-Air.lan>
2021-11-03 22:20:04 +0100

    let's try to add exercises

---
+++ studyProjects/freeCodeCamp/JavaScript Algorithms and Data Structures/caesars-cipher.js
@@ -0,0 +1,19 @@
+/*
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/caesars-cipher
+*/
+function rot13(str) {
+    // 65 - 90
+    var arr = [];
+    for (var i = 0; i < str.length; i++) {
+      if (str.charCodeAt(i) > 64 && str.charCodeAt(i) < 78) {
+        arr.push(String.fromCharCode(str.charCodeAt(i) + 13))
+      }
+      else if (str.charCodeAt(i) > 77 && str.charCodeAt(i) < 91) {
+        arr.push(String.fromCharCode(str.charCodeAt(i) + 13 - 26))
+      }
+      else {arr.push(String.fromCharCode(str.charCodeAt(i)))}
+    }
+    return arr.join("");
+  }
+  
+  rot13("SERR PBQR PNZC");
